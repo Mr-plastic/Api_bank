ed9f72a7316d9cd79bc89a2aaad8179f343f1742
Mr-plastic1 <ppic5413@gmail.com>
2023-02-01 21:08:58 +0800

    Using_struct

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 
 var account = 10000
 
+type Result struct {
+	Money  int    `json:"money"`
+	Status string `json:"status"`
+	Msg    string `json:"message"`
+}
+
+var result = Result{}
+
 func main() {
 	router := gin.Default()
 	router.GET("/account", getBalance)
@@ -19,74 +27,58 @@ func main() {
 }
 
 func getBalance(c *gin.Context) {
-	var msg = "您的帳戶有:" + strconv.Itoa(account) + "元"
-	c.JSON(http.StatusOK, gin.H{
-		"amount":  account,
-		"status":  "OK",
-		"message": msg,
-	})
+	result.Money = account
+	result.Status = "OK"
+	result.Msg = ""
+	c.JSON(http.StatusOK, result)
 }
 
 func deposit(context *gin.Context) {
-	var status string
-	var msg string
-
 	input := context.Param("input")
 	amount, err := strconv.Atoi(input)
 	fmt.Println(err)
 	fmt.Println(amount)
 	if err == nil {
 		if amount <= 0 {
-			amount = 0
-			status = "failed"
-			msg = "操作失敗，存款金額需大於0元！"
+			result.Money = 0
+			result.Status = "failed"
+			result.Msg = "操作失敗，存款需要大於0元"
 		} else {
 			account += amount
-			status = "ok"
-			msg = "已成功存款" + strconv.Itoa(amount) + "元"
+			result.Status = "ok"
+			result.Msg = "已成功存款" + strconv.Itoa(amount) + "元"
 		}
 	} else {
-		amount = 0
-		status = "failed"
-		msg = "操作失敗，輸入有誤！"
+		result.Money = 0
+		result.Status = "failed"
+		result.Msg = "操作失敗，輸入有誤！"
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"amount":  amount,
-		"status":  status,
-		"message": msg,
-	})
+	context.JSON(http.StatusOK, result)
 }
 func withdraw(c *gin.Context) {
-	var msg string
-	var status string
-
 	input := c.Param("input")
 	amount, err := strconv.Atoi(input)
 
 	if err == nil {
 		if amount < 0 {
-			amount = 0
-			msg = "金額不可以小於0，請重新輸入"
-			status = "false"
+			result.Money = 0
+			result.Msg = "金額不可以小於0，請重新輸入"
+			result.Status = "false"
 		} else {
 			if account-amount < 0 {
-				amount = 0
-				status = "failed"
-				msg = "錢不夠!"
+				result.Money = 0
+				result.Status = "failed"
+				result.Msg = "錢不夠!"
 			} else {
 				account = account - amount
-				status = "ok"
-				msg = "成功提款" + strconv.Itoa(amount) + "元"
+				result.Status = "ok"
+				result.Msg = "成功提款" + strconv.Itoa(amount) + "元"
 			}
 		}
 	} else {
-		amount = 0
-		status = "false"
-		msg = "請重新輸入"
+		result.Money = 0
+		result.Status = "false"
+		result.Msg = "請重新輸入"
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"amount":  amount,
-		"status":  status,
-		"message": msg,
-	})
+	c.JSON(http.StatusOK, result)
 }
